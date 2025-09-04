058e6333d830251fdf01b53c7d641eb0e1c2c443
thesubconboi@gmail.com <thesubconboi@gmail.com>
2025-09-03 22:08:09 -0300

    exercicio de contagem

---
+++ ex-simples-contador.py
@@ -0,0 +1,8 @@
+def contagem(num):
+    for numero in range(num, 0, -1):
+        if (numero == 3):
+            print(numero)
+            break
+        else:
+            print(numero)
+contagem(100)
