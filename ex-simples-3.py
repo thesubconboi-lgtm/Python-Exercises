ec80bc8835bc81c8f92e201047ca5e5e51d3d9a6
thesubconboi@gmail.com <thesubconboi@gmail.com>
2025-09-03 22:06:00 -0300

    exercicio de listagem

---
+++ ex-simples-3.py
@@ -0,0 +1,9 @@
+def printLista(lista):
+    for i in lista:
+        if(i == "c"):
+            continue
+        else:
+            print(i)
+listagem = ["a", "b", "c", "d"]
+
+printLista(listagem)
